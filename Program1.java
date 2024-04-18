740ee61fcaf0d298dfed9416b4ace7d9b97b0630
mrfabroa <eric.fabroa@ycdsbk12.ca>
2024-04-18 13:10:51 -0400

    update

--- Program1.java
+++ Program1.java
@@ -1,13 +1,39 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Compute the distance between two points
+  * @author: E. Fabroa
   */
   
   public void run() {
-    // your code goes here
-    
-    
+
+    int intX1, intX2;
+    int intY1, intY2;
+    double dblDist;
+
+    intX1 = readInt("Enter x1: ");
+    intY1 = readInt("Enter Y1: ");
+
+    intX2 = readInt("Enter x2: ");
+    intY2 = readInt("Enter Y2: ");
+
+    dblDist = getDistance(intX1, intY1, intX2, intY2);
+    System.out.println("This distance between points is " + dblDist);
   }
+
+
+ /**
+  * Computes the distance between two points
+  * @param x_1 x value of the first point
+  * @param y_1 y value of the first point
+  * @param x_2 x value of the second point
+  * @param y_2 x value of the second point
+  * @return the distance between the two points
+  */
+  private double getDistance(int x_1, int y_1, int x_2, int y_2){
+    
+    double distance = Math.sqrt(Math.pow(x_2 - x_1,2)+ Math.pow(y_2 - y_1,2));
+    return distance;
+  }
+
 }
---
+++ Program2.java
@@ -0,0 +1,44 @@
+class Program2 extends ConsoleProgram {
+
+  /**
+  * Compute the distance between two points
+  * @author: E. Fabroa
+  */
+
+  int width;
+  int height;
+  
+  public void run(){
+
+    String colour;
+
+    colour = readLine("Enter a colour: ");
+    width = readInt("Enter the width: ");
+
+    height = readInt("Enter the height: ");
+  
+    System.out.println("The " + colour + "rectangle's area is " + getArea());
+    System.out.println("The " + colour + "rectangle's perimeter is " + getPerimeter());
+}
+
+  /**
+   * compute the area of a rectangle
+   * @return the area of the rectangle
+   */
+  private int getArea(){
+    int width2 = 0;
+    return width * height;
+  }
+
+  /**
+   * compute the perimeter of a rectangle
+   * @return the perimeter of the rectangle
+   */
+  private int getPerimeter(){
+    int width2 = width * 2;
+    int height2 = height * 2;
+
+    return width2 + height2;
+  }
+  
+}
